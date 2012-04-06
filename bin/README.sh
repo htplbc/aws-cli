4992ca8ed0f66b75ed99b8cc84bd9ce004ee1b7d
Peter Sagerson <psagers.github@ignorare.net>
2012-04-05 17:00:33 -0700

    Add ec2-region script for quick region switching.

---
+++ bin/README.sh
@@ -0,0 +1,3 @@
+# This directory contains miscellaneous scripts of our own for working with
+# AWS. As much as possible, they should be compatible with zsh function
+# autloading, which means no internal function definitions.
